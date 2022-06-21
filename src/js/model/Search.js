811d5ed2d091536bf38bb8b830c8e308bb0bcb41
choibalsan <choiboosmile@gmail.com>
2022-06-21 20:47:18 +0800

    DOCS folder uusgelee

---
+++ src/js/model/Search.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async doSearch() {
+    try {
+      let result = await axios(
+        "https://forkify-api.herokuapp.com/api/search?q=" + this.query
+      );
+
+      this.recipes = result.data.recipes;
+      return this.recipes;
+    } catch (error) {
+      alert("Асуудал гарлаа " + error);
+    }
+  }
+}
